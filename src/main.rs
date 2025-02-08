0d54b709ae888e4acfe5215084f0f6f446688bc1
Jon Charter <jon@jc-habitat-mbp.local>
2025-02-08 15:09:08 +0000

    foo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use ::tokio;
+use clap::Parser;
+use tokio::signal;
+
 mod reddit_bot;
+
+#[derive(Parser, Debug)]
+struct Opts {}
 
 #[tokio::main]
 async fn main() {
     // let client = reddit_bot::do_client()
     let sub = reddit_bot::read_sub("openuniversity".into()).await;
+    println!("Application running. Press Ctrl+C to exit.");
+    signal::ctrl_c().await.unwrap();
+    println!("Shutting down gracefully...");
 }
